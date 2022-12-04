4586caaef1f650c30d07e06014aa66fb65d7f8f2
LuigiLennon <119767771+LuigiLennon@users.noreply.github.com>
2022-12-03 22:13:23 -0300

    Add files via upload

---
+++ Insertion sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// Função para imprimir uma matriz
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; i++) {
+    printf("%d ", array[i]);
+  }
+  printf("\n");
+}
+
+void insertionSort(int array[], int size) {
+  for (int step = 1; step < size; step++) {
+    int key = array[step];
+    int j = step - 1;
+
+    // Compare a chave com cada elemento à esquerda até que um elemento menor que
+    // ele é encontrado.
+    // Para uma ordem decrescente, altere key<array[j] para key>array[j].
+    while (key < array[j] && j >= 0) {
+      array[j + 1] = array[j];
+      --j;
+    }
+    array[j + 1] = key;
+  }
+}
+
+// Código do driver
+int main() {
+  int data[] = {9, 5, 1, 4, 3};
+  int size = sizeof(data) / sizeof(data[0]);
+  insertionSort(data, size);
+  printf("Sorted array in ascending order:\n");
+  printArray(data, size);
+}
